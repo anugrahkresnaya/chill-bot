8bab90756c226370a8bdf6baccd5a32e7d8b556a
Anugrah Kresnaya <anugrahkresnaya.ak@gmail.com>
2023-01-31 22:14:33 +0700

    refactor: add new guild id

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
-const Discord = require('discord.js');
-const dotenv = require('dotenv');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-const fs = require('fs');
-const { Player } = require('discord-player');
+const Discord = require('discord.js')
+const dotenv = require('dotenv')
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
+const fs = require('fs')
+const { Player } = require('discord-player')
 
-dotenv.config();
-const TOKEN = process.env.TOKEN;
+dotenv.config()
+const TOKEN = process.env.TOKEN
 
-const LOAD_SLASH = process.argv[2] == 'load';
+const LOAD_SLASH = process.argv[2] == 'load'
 
-const CLIENT_ID = '655597195063590912';
-const GUILD_ID = '1020034266542899220';
+const CLIENT_ID = '655597195063590912'
+const GUILD_ID = '1041792287669239938'
 
 const client = new Discord.Client({
-  intents: [
-    'Guilds',
-    'GuildVoiceStates',
-  ]
-});
+  intents: ['Guilds', 'GuildVoiceStates'],
+})
 
-client.slashcommands = new Discord.Collection();
+client.slashcommands = new Discord.Collection()
 client.player = new Player(client, {
   ytdlOptions: {
     quality: 'highestaudio',
-    highWaterMark: 1 << 25
-  }
-});
+    highWaterMark: 1 << 25,
+  },
+})
 
-let commands = [];
+let commands = []
 
-const slashFiles = fs.readdirSync('./slash').filter(file => file.endsWith('.js'));
+const slashFiles = fs
+  .readdirSync('./slash')
+  .filter((file) => file.endsWith('.js'))
 for (const file of slashFiles) {
-  const slashcmd = require(`./slash/${file}`);
-  client.slashcommands.set(slashcmd.data.name, slashcmd);
+  const slashcmd = require(`./slash/${file}`)
+  client.slashcommands.set(slashcmd.data.name, slashcmd)
   if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
 }
 
 if (LOAD_SLASH) {
-  const rest = new REST({ version : '9' }).setToken(TOKEN)
+  const rest = new REST({ version: '9' }).setToken(TOKEN)
   console.log('deploying slash commands')
-  rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
-  .then(() => {
-    console.log('success loaded')
-    process.exit(0)
-  })
-  .catch((err) => {
-    if (err) {
-      console.log(err)
-      process.exit(1)
-    }
-  })
-}
-else {
+  rest
+    .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+      body: commands,
+    })
+    .then(() => {
+      console.log('success loaded')
+      process.exit(0)
+    })
+    .catch((err) => {
+      if (err) {
+        console.log(err)
+        process.exit(1)
+      }
+    })
+} else {
   client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
   })
@@ -60,11 +61,11 @@ else {
     const handlecommand = async () => {
       if (!interaction.isCommand()) return
 
-    const slashcmd = client.slashcommands.get(interaction.commandName)
-    if (!slashcmd) interaction.reply('not a valid slash command')
+      const slashcmd = client.slashcommands.get(interaction.commandName)
+      if (!slashcmd) interaction.reply('not a valid slash command')
 
-    await interaction.deferReply();
-    await slashcmd.run({ client, interaction })
+      await interaction.deferReply()
+      await slashcmd.run({ client, interaction })
     }
     handlecommand()
   })
